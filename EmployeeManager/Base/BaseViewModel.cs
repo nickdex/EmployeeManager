8c964ba03562f6f8057d8d483c4de8ccf3f70411
nickdex <nwarke@protonmail.com>
2017-04-11 08:37:30 +0530

    Add BaseViewModel

---
+++ EmployeeManager/Base/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Base
+{
+    class BaseViewModel
+    {
+        protected void GoToMainWindow()
+        {
+            MainWindow window = new MainWindow();
+            var current = App.Current.MainWindow;
+            App.Current.MainWindow = window;
+
+            window.Show();
+            current.Close();
+        }
+    }
+}
